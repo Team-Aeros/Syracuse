75162ee232728cb4aeacf9eca0c2739d0ace1781
Robert Monden <dev@robertmonden.com>
2018-01-11 13:54:16 +0100

    Added template loader

---
+++ cache/f3/f312a1c04b8bb1e9c84c75d8679cc47aac0cc4c7fa532b24e25aed4689eb3531.php
@@ -0,0 +1,45 @@
+<?php
+
+/* main.tpl */
+class __TwigTemplate_a29cc3150bb6cd73a29b4f3c74151af8e6dddead6714bf86b78dfa5f2ee1a6f9 extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        // line 1
+        echo "<!DOCTYPE HTML>
+<html lang=\"en\">
+    <head>
+        <title>Welcome to Syracuse</title>
+    </head>
+
+    <body>
+
+    </body>
+</html>";
+    }
+
+    public function getTemplateName()
+    {
+        return "main.tpl";
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  19 => 1,);
+    }
+
+    public function getSourceContext()
+    {
+        return new Twig_Source("", "main.tpl", "/srv/http/Syracuse/public/views/delft/templates/main.tpl");
+    }
+}
