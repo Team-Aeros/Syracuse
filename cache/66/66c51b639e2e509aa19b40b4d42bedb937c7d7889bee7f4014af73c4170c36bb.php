e172e4a6c441d83c61809b3f5f8cfd2e96e17b35
Robert Monden <dev@robertmonden.com>
2018-01-11 14:41:14 +0100

    Language loading + Twig

---
+++ cache/66/66c51b639e2e509aa19b40b4d42bedb937c7d7889bee7f4014af73c4170c36bb.php
@@ -0,0 +1,54 @@
+<?php
+
+/* main.tpl */
+class __TwigTemplate_e164e1152557db4e2ba0cda76dcf37f33692cdb207974240298b998565fa70e1 extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        // line 1
+        echo "<!DOCTYPE HTML>
+<html lang=\"en\">
+    <head>
+        <title>Welcome to Syracuse</title>
+    </head>
+
+    <body>
+
+    </body>
+</html>";
+    }
+
+    public function getTemplateName()
+    {
+        return "main.tpl";
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  19 => 1,);
+    }
+
+    public function getSourceContext()
+    {
+        return new Twig_Source("<!DOCTYPE HTML>
+<html lang=\"en\">
+    <head>
+        <title>Welcome to Syracuse</title>
+    </head>
+
+    <body>
+
+    </body>
+</html>", "main.tpl", "/srv/http/Syracuse/public/views/delft/templates/main.tpl");
+    }
+}
--- syracuse.sql
+++ syracuse.sql
@@ -3,7 +3,18 @@ CREATE TABLE IF NOT EXISTS van_setting (
     val TEXT
 );
 
+CREATE TABLE IF NOT EXISTS van_language (
+    id INT UNSIGNED NOT NULL PRIMARY KEY,
+    `name` TINYTEXT NOT NULL,
+    native TINYTEXT NOT NULL,
+    code CHAR(5) NOT NULL
+);
+
+INSERT INTO van_language (id, `name`, native, code) VALUES
+(1, 'English (US)', 'English', 'en_US');
+
 INSERT INTO van_setting (identifier, val) VALUES
 ('theme', 'delft'),
 ('path', '/srv/http/Syracuse'),
-('url', 'http://localhost/Syracuse');
+('url', 'http://localhost/Syracuse'),
+('language', 1);
