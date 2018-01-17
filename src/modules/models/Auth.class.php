a0e8982719d2d36ba4d268c2a447accf59dbce0d
Jelmer Haarman <jelmer.internet@xs4all.nl>
2018-01-17 14:29:04 +0100

    Login systeem toegevoegd

---
+++ src/modules/models/Auth.class.php
@@ -0,0 +1,88 @@
+<?php
+/**
+ * Created by me.
+ * User: Jelmer
+ * Date: 15-Jan-18
+ * Time: 09:56
+ */
+
+namespace Syracuse\src\auth\models;
+
+use Syracuse\src\database\Database;
+use Syracuse\src\headers\ControllerHeader;
+use Syracuse\src\headers\ModelHeader;
+
+class Auth extends ModelHeader {
+
+    private function getName(){
+        $names = Database::interact('retrieve', 'accounts')
+            ->fields('name')
+            ->getAll();
+        $array = $names[0];
+        return $array['name'];
+    }
+
+    private function getPass($name) {
+        $db_name = $name;
+        $pass = Database::interact('retrieve', 'accounts')
+            ->fields('pass')
+            ->where(['name', $db_name])
+            ->getAll();
+        $array = $pass[0];
+        return $array['pass'];
+    }
+
+    private function getSalt() {
+        $salt = Database::interact('retrieve', 'setting')
+            ->fields('identifier', 'val')
+            ->where(['identifier', 'salt'])
+            ->getAll();
+        $Array = $salt[0];
+        $saltArray = $Array['val'];
+        $saltArray2 = ['salt' => $saltArray];
+        return $saltArray2;
+
+    }
+
+    private function hashPass($salt, $pass) {
+        $hashPass = password_hash($pass, PASSWORD_BCRYPT, $salt);
+        return $hashPass;
+    }
+
+    private function errorMsg() {
+        echo "Username or password was incorrect.";
+    }
+
+
+    private function checkCred() {
+        if (!empty($_POST['username'] && !empty($_POST['password']))) {
+            $db_name = $this->getName();
+            if ($_POST['username'] === $db_name) {
+                $db_pass = $this->getPass($db_name);
+                if ($this->hashPass($this->getSalt(), $_POST['password']) === $db_pass) {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+        public function login() {
+            #REMOVE !!!!!!!!!!!!!!!!!!!
+            $_POST['username'] = 'admin';
+            $_POST['password'] = "1234";
+            #REMOVE !!!!!!!!!!!!!!!!!!!
+
+            if($this->checkCred()) {
+                return True;
+            }
+            else {
+                $this->errorMsg();
+                return False;
+            }
+
+        }
+
+}
+
+
+
