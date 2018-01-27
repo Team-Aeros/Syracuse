9b421af7ba09beb24ffb376fef47eb7c92fde26d
Jelmer Haarman <jelmer.internet@xs4all.nl>
2018-01-27 18:18:30 +0100

    Router.class update
    Het werkt
    Met dank aan Robert

--- src/core/controllers/Router.class.php
+++ src/core/controllers/Router.class.php
@@ -1,54 +1,49 @@
 <?php
 namespace Syracuse\src\core\controllers;
 class Router {
+
     private $routes = array();
-    public function addRoute($parameters, $request, $handler) {
+
+    public const RETURN_FOUND = 0x00;
+    public const RETURN_NOT_FOUND = 0x01;
+
+    public function addRoute($requestMethod, $request, $handler) {
         $id = sizeof($this->routes);
-        $request = explode("/",$request);
-        unset($request[0]);
-        $route = array($id, $parameters, $request, $handler);
+        $request = array_slice(explode("/", $request), 1);
+        $route = array($id, $requestMethod, $request, $handler);
         $this->routes[] = $route;
     }
 
-
     public function dispatch($requestMethod, $uri) {
-        $found = False;
-        foreach ($this->routes as $route ) {
-            $parameters = $route[1];
-            #print_r($parameters);
-
-            $urlParts = explode("/", $uri);
-            unset($urlParts[0]);
-            #print_r($urlParts);
-
+        foreach ($this->routes as $route) {
+            $urlParts = array_slice(explode("/", $uri), 1);
             $request = $route[2];
-            #print_r($request);
 
-            #print_r($route);
+            $parameters = [];
 
-            if(in_array($requestMethod, $parameters)) {
-                foreach ($urlParts as $part){
-                    $par = array();
-                    #print_r($part);
-                    if($part[0] == "{" and $part[strlen($part)-1] == "}") {
-                        $par[] = substr($part, 1, -1);
+            if(in_array($requestMethod, is_array($route[1]) ? $route[1] : [$route[1]])) {
+                if (empty($urlParts))
+                    continue;
 
-                    }
-                    #print_r($par);
-                    if(!in_array($part, $request)) {
-                        break 1;
-                    } else {
-                        if($part == $urlParts[sizeof($urlParts)]) {
-                            $returnArray = array(0, $urlParts[0], $par);
-                            return $returnArray;
-                        }
-                    }
+                foreach ($urlParts as $key => $part){
+                    if (empty($part))
+                        continue 2;
+
+                    if (empty($request[$key]) || ($request[$key] != $part && $request[$key][0] != '{' && $request[$key][strlen($request[$key]) - 1] != '}'))
+                        continue 2;
+
+                    if ($request[$key][0] == '{' && $request[$key][strlen($request[$key]) - 1] == '}')
+                        $parameters[substr($request[$key], 1, strlen($request[$key]) - 2)] = $part;
                 }
-            }
 
+                return [
+                    self::RETURN_FOUND,
+                    $route[3],
+                    $parameters
+                ];
+            }
         }
-        if(!$found) {
-            return array(1);
-        }
+
+        return self::RETURN_NOT_FOUND;
     }
 }
