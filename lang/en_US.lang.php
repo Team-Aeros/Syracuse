467f38e25d7436b99e96fa154bad3066936b2bee
Jelmer Haarman <jelmer.internet@xs4all.nl>
2018-01-28 13:40:14 +0100

    Help pagina structuur verandert

--- lang/en_US.lang.php
+++ lang/en_US.lang.php
@@ -7,11 +7,18 @@ return [
     'not_implemented' => 'This feature has not been implemented yet',
 
     'help' => 'Help',
-    'help_body' => '
-        <p>Welcome to the help page. If you need any assistance with using Syracuse, this is where you need to be.</p>
-        
-        <h1>Logging in</h1>
-        <p>Before you can view the dashboard, you need to log in.</p>',
+    'helpStart' => 'Welcome to the help page. If you need any assistance with using Syracuse, this is where you need to be.',
+    'subLogin' => 'Logging in',
+    'exLogin' => 'Before you can view the dashboard, you need to log in.',
+    'subData' => 'On this website you have acces to the following data:',
+    'exData1' => 'Top 10 list of weatherstations where it rained the most on the current day.',
+    'exData2' => 'When clicked on a station in this list you can see the min and max temperature of the current day and previous 6 days measured in Celsius, 
+        the amount of rainfall and the max wind speed measured in km/h of that weather station.',
+    'exData3'=> 'A map of all the weather stations in the Gulf of Mexico, when a weather stations is clicked a graph showing the temperature from the past hour wil be displayed.',
+    'subComp' => 'Aeros Development.',
+    'exComp' => 'This website was made by Aeros Development, a small company experienced in programming, designing interfaces and data handeling',
+
+
     'download' => 'Download',
     'logout' => 'Log out',
     'selStat' => 'No station selected',
